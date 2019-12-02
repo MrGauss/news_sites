e7d278b13543c99a15d9ca986c287709270e83c0
MrGauss <mail4gauss@gmail.com>
2019-12-02 19:20:15 +0200

    autoupdate 2019-12-02-19:20:15

--- cache/cache-visitors-20191202-1315.cknews.pp.ua.php
+++
@@ -1,4 +0,0 @@
-<?php if( !defined('GAUSS_CMS') ){ echo basename(__FILE__); exit; }
- /* CACHE CREATED: 1575285413.5557 (2019-12-02 13:16:53) */
-return '39'; 
-?>
--- cache/cache-visitors-20191202-1315.it-news.pp.ua.php
+++
@@ -1,4 +0,0 @@
-<?php if( !defined('GAUSS_CMS') ){ echo basename(__FILE__); exit; }
- /* CACHE CREATED: 1575285458.9518 (2019-12-02 13:17:38) */
-return '48'; 
-?>
