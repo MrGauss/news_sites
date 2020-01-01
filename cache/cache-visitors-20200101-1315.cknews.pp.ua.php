f788439c2301fd7231851e5886869986151809c2
MrGauss <mail4gauss@gmail.com>
2020-01-01 19:20:22 +0200

    autoupdate 2020-01-01-19:20:22

--- cache/cache-visitors-20200101-1315.cknews.pp.ua.php
+++
@@ -1,4 +0,0 @@
-<?php if( !defined('GAUSS_CMS') ){ echo basename(__FILE__); exit; }
- /* CACHE CREATED: 1577877674.9642 (2020-01-01 13:21:14) */
-return '62'; 
-?>
--- cache/cache-visitors-20200101-1315.it-news.pp.ua.php
+++
@@ -1,4 +0,0 @@
-<?php if( !defined('GAUSS_CMS') ){ echo basename(__FILE__); exit; }
- /* CACHE CREATED: 1577877539.3285 (2020-01-01 13:18:59) */
-return '14'; 
-?>
